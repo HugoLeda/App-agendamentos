b220ddb850cd366e539d71470ad8189a522c6bc5
João Hugo Leda <joaohugoleda2@gmail.com>
2021-01-26 01:58:23 -0300

    Init

---
+++ Agendei.dpr
@@ -0,0 +1,14 @@
+program Agendei;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  UnitLogin in '..\..\..\Embarcadero\Studio\Projects\UnitLogin.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
